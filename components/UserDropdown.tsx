308202f8efae0975db29a0cfc9c60bd0bc250a83
Ayoade <ayoadesunday97@gmail.com>
2025-10-13 16:09:01 +0100

    Implement search stock

--- components/UserDropdown.tsx
+++ components/UserDropdown.tsx
@@ -15,7 +15,7 @@ import {LogOut} from "lucide-react";
 import NavItems from "@/components/NavItems";
 import { signOut } from "@/lib/actions/auth.actions";
 
-const UserDropdown = ({ user }: { user: User }) => {
+const UserDropdown = ({ user, initialStocks }: { user: User, initialStocks: StockWithWatchlistStatus[] }) => {
     const router = useRouter();
 
     const handleSignOut = async () => {
@@ -60,7 +60,7 @@ const UserDropdown = ({ user }: { user: User }) => {
                 </DropdownMenuItem>
                 <DropdownMenuSeparator className='bg-gray-600 hidden sm:block' />
                 <nav className='sm:hidden'>
-                    <NavItems />
+                    <NavItems initialStocks={initialStocks} />
                 </nav>
             </DropdownMenuContent>
         </DropdownMenu>
