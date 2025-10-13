dfe8141e69c94c49543bb3a79d81f5e9c7e4857a
Ayoade <ayoadesunday97@gmail.com>
2025-10-13 13:52:24 +0100

    Implement auth logic

--- app/(auth)/sign-up/page.tsx
+++ app/(auth)/sign-up/page.tsx
@@ -7,8 +7,12 @@ import SelectField from "@/components/forms/SelectField";
 import {INVESTMENT_GOALS, PREFERRED_INDUSTRIES, RISK_TOLERANCE_OPTIONS} from "@/lib/constants";
 import {CountrySelectField} from "@/components/forms/CountrySelectField";
 import FooterLink from "@/components/forms/FooterLink";
+import { signUpWithEmail } from "@/lib/actions/auth.actions";
+import { useRouter } from "next/navigation";
+import { toast } from "sonner";
 
 const SignUp = () => {
+    const router = useRouter();
     const {
         register,
         handleSubmit,
@@ -27,12 +31,15 @@ const SignUp = () => {
         mode: 'onBlur'
     })
 
-    const onSubmit = async(data: SignUpFormData) => {
-        console.log('Submitting form data:', data);
-        try{
-            console.log(data)
-        }catch(error){
-            console.error(error)
+    const onSubmit = async (data: SignUpFormData) => {
+        try {
+            const result = await signUpWithEmail(data);
+            if(result.success) router.push('/');
+        } catch (e) {
+            console.error(e);
+            toast.error('Sign up failed', {
+                description: e instanceof Error ? e.message : 'Failed to create an account.'
+            })
         }
     }
 
--- components/Header.tsx
+++ components/Header.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 import NavItems from "@/components/NavItems";
 import UserDropdown from "@/components/UserDropdown";
 
-const Header = () => {
+const Header = ({ user }: { user: User }) => {
     return (
         <header className='sticky top-0 header'>
             <div className='container header-wrapper'>
@@ -13,7 +13,7 @@ const Header = () => {
                 <nav className='hidden sm:block'>
                     <NavItems />
                 </nav>
-                <UserDropdown />
+                <UserDropdown user={user} />
             </div>
         </header>
     )
